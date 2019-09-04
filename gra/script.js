cc6e9f9ffd57c63c79f62726af4477a5388704aa
MaciekBe89 <maciejb@virtualdesign.pl>
2019-09-04 13:14:01 +0200

    width / height cell

--- gra/script.js
+++ gra/script.js
@@ -2,4 +2,6 @@ function createCell(x, y) {
     const cell = document.createElement('div');
     cell.className = "square";
     document.body.appendChild(cell);
+    cell.style.width = '20px';
+    cell.style.height = '20px';
 }
