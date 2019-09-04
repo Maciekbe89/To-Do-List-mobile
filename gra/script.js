0edb6a0d29f574c28ae87f86bb3d4d7c3fa46eb3
MaciekBe89 <maciejb@virtualdesign.pl>
2019-09-04 13:28:10 +0200

    position square

--- gra/script.js
+++ gra/script.js
@@ -1,7 +1,11 @@
+const cellSize = 20;
+
 function createCell(x, y) {
     const cell = document.createElement('div');
     cell.className = "square";
     document.body.appendChild(cell);
-    cell.style.width = '20px';
-    cell.style.height = '20px';
+    cell.style.width = cellSize + 'px';
+    cell.style.height = cellSize + 'px';
+    cell.style.top = cellSize * y + 'px';
+    cell.style.left = cellSize * x + 'px';
 }
