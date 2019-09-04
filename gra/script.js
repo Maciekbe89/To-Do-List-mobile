beb46718d90741de3d6089335ce079d525c37557
MaciekBe89 <maciejb@virtualdesign.pl>
2019-09-04 13:58:58 +0200

    drawBoard

--- gra/script.js
+++ gra/script.js
@@ -1,5 +1,6 @@
 const cellSize = 20;
 
+
 function createCell(x, y) {
     const cell = document.createElement('div');
     cell.className = "square";
@@ -9,3 +10,17 @@ function createCell(x, y) {
     cell.style.top = cellSize * y + 'px';
     cell.style.left = cellSize * x + 'px';
 }
+
+function drawBoard(maxX, maxY) {
+    for (x = 0; x < maxX; x++) {
+        for (y = 0; y < maxY; y++) {
+            createCell(x, y);
+        }
+    }
+}
+
+function main() {
+    drawBoard(10, 10);
+}
+
+window.onload = main;
