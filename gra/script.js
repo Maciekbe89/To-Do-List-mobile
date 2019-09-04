28b72702e49d6322ecfe6368aacef2c699402ebe
MaciekBe89 <maciejb@virtualdesign.pl>
2019-09-04 14:30:47 +0200

    class active

--- gra/script.js
+++ gra/script.js
@@ -9,6 +9,8 @@ function createCell(x, y) {
     cell.style.height = cellSize + 'px';
     cell.style.top = cellSize * y + 'px';
     cell.style.left = cellSize * x + 'px';
+    cell.dataset.x = x;
+    cell.dataset.y = y;
 }
 
 function drawBoard(maxX, maxY) {
@@ -19,8 +21,20 @@ function drawBoard(maxX, maxY) {
     }
 }
 
+function setActive(x, y) {
+    const cell = document.querySelector(`[data-x="${x}"][data-y="${y}"]`);
+    cell.classList.add('active');
+}
+
+function setUnActive(x, y) {
+    const cell = document.querySelector(`[data-x="${x}"][data-y="${y}"]`);
+    cell.classList.remove('active');
+}
+
 function main() {
     drawBoard(10, 10);
 }
+
+
 
 window.onload = main;
