5c4882f102468e9d67a19f58f4eb784d59be1eac
MaciekBe89 <maciejb@virtualdesign.pl>
2019-09-27 20:09:44 +0200

    JSON

--- TO DO LIST mobile/script.js
+++ TO DO LIST mobile/script.js
@@ -29,13 +29,27 @@ const addTask = (e) => {
     task.querySelector('button').addEventListener('click', removeTask);
 }
 
+const addTaskFromStorage = (titleTask) => {
+    if (titleTask === '') return;
+    const task = document.createElement('li');
+    task.className = 'task';
+    task.innerHTML = '<i class="fas fa-dot-circle"></i>' + titleTask + ' <button><i class="fas fa-minus-circle"></i></button>';
+    toDoList.push(task);
+    renderList();
+    ul.appendChild(task);
+    taskNumber.textContent = listItems.length;
+    task.querySelector('button').addEventListener('click', removeTask);
+}
+
 const renderList = () => {
     ul.textContent = "";
     toDoList.forEach((toDoElement, key) => {
         toDoElement.dataset.key = key;
         ul.appendChild(toDoElement);
         taskNumber.textContent = listItems.length;
-    })
+    });
+    const toDoData = toDoList.map(e => e.textContent);
+    localStorage.setItem('todo', JSON.stringify(toDoData));
 }
 
 const inputSearch = document.querySelector('input.search');
@@ -50,3 +64,8 @@ const searchTask = (e) => {
 
 inputSearch.addEventListener('input', searchTask);
 form.addEventListener('submit', addTask);
+
+window.onload = () => {
+    const tasks = JSON.parse(localStorage.getItem('todo'));
+    tasks.forEach(task => addTaskFromStorage(task));
+}
